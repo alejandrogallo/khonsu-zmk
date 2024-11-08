6bffe6d93b13b3618e0b414ddac6e5c63c61fd98
Alejandro Gallo <aamsgallo@gmail.com>
2024-11-08 20:46:34 +0100

    Update

---
+++ Makefile
@@ -0,0 +1,6 @@
+./config/boards/shields/corne/corne.keymap: keymap.el crkbd.el
+	emacs -Q --batch --load keymap.el --load crkbd.el
+
+./config/boards/shields/khonsu/khonsu.keymap: keymap.el khonsu.el
+	emacs -Q --batch --load keymap.el --load khonsu.el
+
---
+++ crkbd.el
@@ -0,0 +1,24 @@
+(require 'kh)
+
+(defun kh-crkbd-pad-layer (layer)
+  (pcase layer
+    (`(:left (,row1-l ,row2-l ,row3-l ,row4-l)
+             :right (,row1-r ,row2-r ,row3-r ,row4-r))
+      (list :left (list (cons '(:trans) row1-l)
+                        (cons '(:trans) row2-l)
+                        (cons '(:trans) row3-l)
+                        (cons '(:na) row4-l))
+            :right (list (cons '(:trans) row1-r)
+                         (cons '(:trans) row2-r)
+                         (cons '(:trans) row3-r)
+                         (cons '(:na) row4-r))))))
+
+(defun kh-crkbd-pad-keymap (keymap-alist)
+  (cl-loop for (name . layer) in keymap-alist
+        collect (cons name (kh-crkbd-pad-layer layer))))
+
+(defvar kh-crkbd-keymap-alist (kh-crkbd-pad-keymap kh-keymap ))
+
+(kh-render-zmk-keymap-file kh-combos
+                           kh-crkbd-keymap-alist
+                           "./config/boards/shields/corne/corne.keymap")
